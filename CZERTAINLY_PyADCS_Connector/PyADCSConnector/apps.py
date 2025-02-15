4153bb5f699bc61080203dc8ffc13a21a8a7dc5d
3keyroman <roman.cinkais@3key.company>
2025-02-15 23:17:17 +0100

    Update database connection to support PgBouncer

--- CZERTAINLY_PyADCS_Connector/PyADCSConnector/apps.py
+++ CZERTAINLY_PyADCS_Connector/PyADCSConnector/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PyADCSConnectorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'PyADCSConnector'
+
+    def ready(self):
+        import PyADCSConnector.signals
